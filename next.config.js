772bea1bb4d45296259817072d964c85f0edc913
yokoyamahirokazu <h_yokoyama@side-creative.com>
2021-11-26 09:26:57 +0900

    リダイレクトとdescpriprion変更

--- next.config.js
+++ next.config.js
@@ -2,4 +2,58 @@ module.exports = {
   images: {
     domains: ['images.microcms-assets.io'],
   },
+  async redirects() {
+    return [
+      {
+        source: '/download/whitepaper/2y-i762tp', // リダイレクト元のURL
+        destination: '/download/2y-i762tp', // リダイレクト先のURL
+        permanent: true, // 永続的なリダイレクトかのフラグ
+      },
+      {
+        source: '/download/whitepaper/hil-rtm3inz8', // リダイレクト元のURL
+        destination: '/download/hil-rtm3inz8', // リダイレクト先のURL
+        permanent: true, // 永続的なリダイレクトかのフラグ
+      },
+      {
+        source: '/download/whitepaper/5snr1ql_3', // リダイレクト元のURL
+        destination: '/download/5snr1ql_3', // リダイレクト先のURL
+        permanent: true, // 永続的なリダイレクトかのフラグ
+      },
+      {
+        source: '/download/whitepaper/4tpxgx6g79', // リダイレクト元のURL
+        destination: '/download/4tpxgx6g79', // リダイレクト先のURL
+        permanent: true, // 永続的なリダイレクトかのフラグ
+      },
+      {
+        source: '/download/whitepaper/yku1q3pbo5', // リダイレクト元のURL
+        destination: '/download/yku1q3pbo5', // リダイレクト先のURL
+        permanent: true, // 永続的なリダイレクトかのフラグ
+      },
+      {
+        source: '/download/whitepaper/mmwa8sv5zba8', // リダイレクト元のURL
+        destination: '/download/mmwa8sv5zba8', // リダイレクト先のURL
+        permanent: true, // 永続的なリダイレクトかのフラグ
+      },
+      {
+        source: '/download/whitepaper/u27m2-8_es', // リダイレクト元のURL
+        destination: '/download/u27m2-8_es', // リダイレクト先のURL
+        permanent: true, // 永続的なリダイレクトかのフラグ
+      },
+      {
+        source: '/download/whitepaper/c21xl2h2pp7', // リダイレクト元のURL
+        destination: '/download/c21xl2h2pp7', // リダイレクト先のURL
+        permanent: true, // 永続的なリダイレクトかのフラグ
+      },
+      {
+        source: '/download/whitepaper/32tupxjzf', // リダイレクト元のURL
+        destination: '/download/32tupxjzf', // リダイレクト先のURL
+        permanent: true, // 永続的なリダイレクトかのフラグ
+      },
+      {
+        source: '/download/whitepaper/8t-k-q4hmlav', // リダイレクト元のURL
+        destination: '/download/8t-k-q4hmlav', // リダイレクト先のURL
+        permanent: true, // 永続的なリダイレクトかのフラグ
+      },
+    ];
+  },
 };
