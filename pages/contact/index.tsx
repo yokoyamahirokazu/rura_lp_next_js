899d4bd8c46437f1e9745652b3e51e8acaf9812a
yokoyamahirokazu <h_yokoyama@side-creative.com>
2021-12-09 16:40:08 +0900

    reCaptcha demo

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -125,6 +125,12 @@ const Index: NextPage = () => {
                 </div>
                 <input type={'hidden'} name={'リードソース'} value={'問い合わせフォーム'} />
                 <div>
+                  <div
+                    className="g-recaptcha"
+                    data-sitekey="6LeonngdAAAAAFhZcAqOlCzr-G5gXGEVUPpWjQIo"
+                  ></div>
+                </div>
+                <div>
                   {/* <button
                   type={'submit'}
                   data-formrun-error-text={'未入力の項目があります'}
