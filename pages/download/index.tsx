f51296eb44c2fc65b123a432281894beaff7f892
yokoyamahirokazu <h_yokoyama@side-creative.com>
2021-12-15 16:23:26 +0900

    download -> hubspot

--- pages/download/index.tsx
+++ pages/download/index.tsx
@@ -1,11 +1,11 @@
 import { NextPage } from 'next';
-import Script from 'react-load-script';
-import React, { useCallback } from 'react';
-import { useTabIndex } from 'react-tabindex';
+import React from 'react';
 import styles from '@styles/components/Components.module.css';
-import Button from '@components/Button';
 import Image from 'next/image';
 import SeoContent from '@components/SeoContent';
+import HubspotForm from 'react-hubspot-form'
+
+
 
 declare global {
   interface Window {
@@ -14,10 +14,7 @@ declare global {
 }
 
 const Index: NextPage = () => {
-  const tabIndex = useTabIndex();
-  const onLoadFormrun = useCallback(() => {
-    window.Formrun?.init('.formrun');
-  }, []);
+
 
   return (
     <>
@@ -76,107 +73,11 @@ const Index: NextPage = () => {
         <div className={styles.contactPageFlexRight}>
           <div className={styles.contactPageFlexInner}>
             <div className={styles.contactContent}>
-              <Script onLoad={onLoadFormrun} url="https://sdk.form.run/js/v2/formrun.js"></Script>
-              <form
-                className={'formrun'}
-                action={'https://form.run/api/v1/r/jla1gd46a72v0m3g4b71smxh'}
-                method={'post'}
-              >
-                <div className={styles.formContentBox}>
-                  <label>
-                    会社名<span className={styles.required}>必須</span>
-                  </label>
-                  <input name={'会社名'} type={'text'} data-formrun-required />
-                  <div className={styles.errorText} data-formrun-show-if-error={'会社名'}>
-                    お名前を入力してください
-                  </div>
-                </div>
-                <div className={styles.formContentBoxFlex}>
-                  <div className={styles.formContentBox}>
-                    <label>
-                      部署<span>任意</span>
-                    </label>
-                    <input name={'部署'} type={'text'} />
-                  </div>
-                  <div className={styles.formContentBox}>
-                    <label>
-                      役職<span>任意</span>
-                    </label>
-                    <input name={'役職'} type={'text'} />
-                  </div>
-                </div>
-                <div className={styles.formContentBox}>
-                  <label>
-                    お名前<span className={styles.required}>必須</span>
-                  </label>
-                  <input name={'お名前'} type={'text'} data-formrun-required />
-                  <div className={styles.errorText} data-formrun-show-if-error={'お名前'}>
-                    お名前を入力してください
-                  </div>
-                </div>
-                <div className={styles.formContentBox}>
-                  <label>
-                    メールアドレス<span className={styles.required}>必須</span>
-                  </label>
-                  <input
-                    name={'メールアドレス'}
-                    type={'text'}
-                    data-formrun-type={'email'}
-                    data-formrun-required
-                  />
-                  <div className={styles.errorText} data-formrun-show-if-error={'メールアドレス'}>
-                    メールアドレスを正しく入力してください
-                  </div>
-                </div>
-                <div className={styles.formContentBox}>
-                  <label>
-                    電話番号<span className={styles.required}>必須</span>
-                  </label>
-                  <input
-                    name={'電話番号'}
-                    type={'text'}
-                    data-formrun-type={'tel'}
-                    data-formrun-required
-                  />
-                  <div className={styles.errorText} data-formrun-show-if-error={'電話番号'}>
-                    電話番号を入力してください
-                  </div>
-                </div>
-                <div className={styles.formContentBox}>
-                  <label>
-                    コメント<span>任意</span>
-                  </label>
-                  <textarea
-                    name={'コメント'}
-                    placeholder={'ご質問・ご要望等ございましたらご記入ください。'}
-                  ></textarea>
-                  <div className={styles.errorText} data-formrun-show-if-error={'コメント'}>
-                    お問い合わせ入力してください
-                  </div>
-                </div>
-
-                <div className={styles._formrun_gotcha}>
-                  <label>If you are a human, ignore this field</label>
-                  <input
-                    type={'text'}
-                    name={'_formrun_gotcha'}
-                    id={'_formrun_gotcha'}
-                    tabIndex={tabIndex}
-                  />
-                </div>
-                <input type={'hidden'} name={'リードソース'} value={'問い合わせフォーム'} />
-                <div>
-                  <Button
-                    bgColor="primary"
-                    size="large"
-                    types="submit"
-                    errorText={'未入力の項目があります'}
-                    submittingText={'送信中...'}
-                  >
-                    送信する
-                  </Button>
-                </div>
-              </form>
+              <HubspotForm
+              portalId='21136941'
+              formId='df368756-6d5c-48a7-b00b-7bd889cbcf0c'
+              loading={<div>Loading...</div>}
+              />
             </div>
           </div>
         </div>
